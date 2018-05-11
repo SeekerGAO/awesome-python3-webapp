5cde7b01a23090db7a151016613e0c2ad3eab467
SeekerGAO <1532584997@qq.com>
2018-05-11 16:16:28 +0800

    ORM

--- www/app.py
+++ www/app.py
@@ -9,13 +9,16 @@ async web application.
 
 import logging; logging.basicConfig(level=logging.INFO)
 
+
 import asyncio, os, json, time
 from datetime import datetime
-
 from aiohttp import web
+
+
 
 def index(request):
     return web.Response(body=b'<h1>Awesome</h1>',headers={'content-type':'text/html'})
+
 
 @asyncio.coroutine
 def init(loop):
@@ -24,6 +27,7 @@ def init(loop):
     srv = yield from loop.create_server(app.make_handler(), '127.0.0.1', 9000)
     logging.info('server started at http://127.0.0.1:9000...')
     return srv
+
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(init(loop))
